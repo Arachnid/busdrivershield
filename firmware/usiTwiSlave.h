a1face9fc7c8ecbaeaaecdcd87a9d582059c557e
Nick Johnson <arachnid@notdot.net>
2011-07-20 16:50:10 +1000

    Added I2C slave library

---
+++ firmware/usiTwiSlave.h
@@ -0,0 +1,50 @@
+/********************************************************************************
+
+Header file for the USI TWI Slave driver.
+
+Created by Donald R. Blake
+donblake at worldnet.att.net
+
+---------------------------------------------------------------------------------
+
+Created from Atmel source files for Application Note AVR312: Using the USI Module
+as an I2C slave.
+
+This program is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+---------------------------------------------------------------------------------
+
+Change Activity:
+
+    Date       Description
+   ------      -------------
+  15 Mar 2007  Created.
+
+********************************************************************************/
+
+
+
+#ifndef _USI_TWI_SLAVE_H_
+#define _USI_TWI_SLAVE_H_
+
+
+/********************************************************************************
+
+                                   prototypes
+
+********************************************************************************/
+
+void    usiTwiSlaveInit( 
+	uint8_t,  	
+	uint8_t	(*onI2CReadFromRegister)(uint8_t reg),
+	void (*onI2CWriteToRegister)(uint8_t reg, uint8_t value)
+ );
+
+#endif  // ifndef _USI_TWI_SLAVE_H_
