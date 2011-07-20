fb9c0766334829bcf415f7c83783e570d1d28cb2
Nick Johnson <arachnid@notdot.net>
2011-07-20 16:50:26 +1000

    Added preliminary register definitions

---
+++ firmware/main.c
@@ -0,0 +1,46 @@
+#include <avr/io.h>
+#include "usiTwiSlave.h"
+
+// Flags for the status register
+#define STATUS_M1_I1 0
+#define STATUS_M1_I2 1
+#define STATUS_M2_I1 2
+#define STATUS_M2_I2 3
+#define STATUS_INT1 4
+#define STATUS_INT2 5
+
+// Flags for the direction register
+#define DIR_M1_CCW 0
+#define DIR_M1_CW 1
+#define DIR_M2_CCW 2
+#define DIR_M2_CW 3
+
+// Flags for the inopts register
+#define INOPT_PULLUP_I1 0
+#define INOPT_PULLUP_I2 1
+#define INOPT_INVERT_I1 2
+#define INOPT_INVERT_I2 3
+#define INOPT_LIMIT_I1 4
+#define INOPT_LIMIT_I2 5
+
+// Flags for the int_masks register
+#define IMASK_LIMIT_M1_I1 0
+#define IMASK_LIMIT_M1_I2 1
+#define IMASK_LIMIT_M2_I1 2
+#define IMASK_LIMIT_M2_I2 3
+
+union {
+    uint8_t bytes[10];
+    struct {
+        uint8_t slave_addr;
+        uint8_t status;         // Input and interrupt status register
+        uint8_t direction;      // Motor direction register
+        uint8_t reserved;       // Reserved for future use
+        uint8_t speeds[2];      // Motor speeds, 0=off/idle, 255=full speed
+        uint8_t inopts[2];      // Input options/flags registers
+        uint8_t int_masks[2];   // Interrupt masks registers
+    } __attribute__((__packed__)) reg;
+} registers;
+
+int main() {
+}
